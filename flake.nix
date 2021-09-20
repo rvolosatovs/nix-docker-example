4b7bea8c6a07886f342f3364912dce8d4e7436d4
Roman Volosatovs <rvolosatovs@riseup.net>
2021-09-20 15:09:17 +0200

    flake: init

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Build OCI images";
+
+  inputs.nixpkgs.url = github:rvolosatovs/nixpkgs;
+
+  outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux; {
+    packages.x86_64-linux.golang = dockerTools.buildImage {
+      name = "golang";
+      tag = "1.17";
+      contents = go_1_17;
+    };
+
+    packages.x86_64-linux.golangWithPython = dockerTools.buildImage {
+      name = "golang-with-python";
+      tag = "latest"; # TODO: figure out how versioning would work here
+      contents = [
+        go_1_17
+        (python.withPackages(pkgs: with pkgs; [
+          numpy
+        ]))
+      ];
+    };
+  };
+}
